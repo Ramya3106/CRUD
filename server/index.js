6deaf7267091d52e38688328047181354a329cdc
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-29 20:27:16 +0530

    updated at index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const Users = require("./sample.json");
+const fs = require("fs");
 const app = express();
 const port = 8000;
 
@@ -18,7 +19,10 @@ app.get("/Users", (req, res) => {
 //Delete User Detail
 app.delete("/Users/:id", (req, res) => {
   let id = Number(req.params.id);
-  let filteredUsers = Users.filter((Users) => User.id !== id);
+  let filteredUsers = Users.filter((User) => User.id !== id);
+  fs.writeFile("./sample.json", JSON.stringify(filteredUsers), (err, data) => {
+    return res.json(filteredUsers);
+  });
 });
 
 app.listen(port, (err) => {
